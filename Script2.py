19a801edb15186b67a8a20043edf10744bf724f5
VirgoSupercluster123 <98437428+VirgoSupercluster123@users.noreply.github.com>
2022-05-27 15:52:51 -0400

    Revert "New changes"
    This reverts commit 64c856e63c2c01e11c2fc5669f1990e4def4c0c3.

--- Script2.py
+++ Script2.py
@@ -1,3 +1,2 @@
-# Learn the ways of the Jedi young padiwan.
 print("If the implementation is hard to explain, it's a bad idea.")
 print("If the implementation is easy to explain, it may be a good idea.")
