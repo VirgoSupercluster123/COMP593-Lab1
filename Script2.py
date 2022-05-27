157be0da4245aeed4480c969ad563cd7e62f5d7b
VirgoSupercluster123 <98437428+VirgoSupercluster123@users.noreply.github.com>
2022-05-27 16:11:00 -0400

    Modified both files in the repo clone
    -Deleted a line in Script1.py.
    - Added two new lines in Script2.py.

--- Script2.py
+++ Script2.py
@@ -1,3 +1,5 @@
 # Learn the ways of the Jedi young padiwan.
 print("If the implementation is hard to explain, it's a bad idea.")
 print("If the implementation is easy to explain, it may be a good idea.")
+print("Jeremy Dalby is my teacher.")
+print("I like too code!")
